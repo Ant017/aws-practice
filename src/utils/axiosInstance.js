c3553ba6c0e0ff13c20e1fabbf697c290558d16c
Ant017 <antika.noor98@gmail.com>
2023-10-30 23:13:32 +0600

    second commit

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://13.39.106.231:8000",
     timeout: 10000
 })
 
 const axiosInstanceToken = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://13.39.106.231:8000",
     timeout: 10000
 })
 
